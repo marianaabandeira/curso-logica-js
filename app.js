557d208c1c4171793ba5dc589fe44c601e137cfe
Mariana Bandeira <marianabandeira.cicm@gmail.com>
2025-07-04 00:05:25 -0300

    atualizando o projeto com novas alterações

--- app.js
+++ app.js
@@ -6,24 +6,3 @@ if (chute == numeroSecreto) {
     console.log('Uhuu! Você descobriu o número secreto! (5)');
 }
 
-
-#Desafio
-
-alert('Boas vindas ao nosso site!');
-
-let nome = 'Lua';
-let idade = 25;
-let numerodeVendas = 50;
-let saldoDisponivel = 1000;
-
-alert('Erro! Preencha todos os campos');
-
-let mensagemDeErro = ('Erro! Preencha todos os campos');
-alert(mensagemDeErro);
-
-nome = prompt('Qual seu nome?');
-idade = number(prompt('Qual sua idade?'));
-
-if (idade >= 18 ) {
-    alerta('Pode tirar a habilitação!');
-} 
