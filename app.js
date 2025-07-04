95c93ba335c4858e169538674a05933b8a8baccf
Mariana Bandeira <marianabandeira.cicm@gmail.com>
2025-07-04 00:02:04 -0300

    primeiro commit do curso de JS

---
+++ app.js
@@ -0,0 +1,29 @@
+alert('Olá mundo, bem-vindo ao jogo do número secreto'); 
+let numeroSecreto = 5;
+let chute = prompt ('Escolha um número entre 1 e 30')
+
+if (chute == numeroSecreto) {
+    console.log('Uhuu! Você descobriu o número secreto! (5)');
+}
+
+
+#Desafio
+
+alert('Boas vindas ao nosso site!');
+
+let nome = 'Lua';
+let idade = 25;
+let numerodeVendas = 50;
+let saldoDisponivel = 1000;
+
+alert('Erro! Preencha todos os campos');
+
+let mensagemDeErro = ('Erro! Preencha todos os campos');
+alert(mensagemDeErro);
+
+nome = prompt('Qual seu nome?');
+idade = number(prompt('Qual sua idade?'));
+
+if (idade >= 18 ) {
+    alerta('Pode tirar a habilitação!');
+} 
