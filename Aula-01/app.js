688bd6b9e8986200ba3e11e4b59679159839e554
Mariana Bandeira <marianabandeira.cicm@gmail.com>
2025-07-10 23:06:03 -0300

    Delete Aula-01 directory

--- Aula-01/app.js
+++
@@ -1,8 +0,0 @@
-alert('Olá mundo, bem-vindo ao jogo do número secreto'); 
-let numeroSecreto = 5;
-let chute = prompt ('Escolha um número entre 1 e 30')
-
-if (chute == numeroSecreto) {
-    console.log('Uhuu! Você descobriu o número secreto! (5)');
-}
-
